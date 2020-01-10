35a062228ddb98882213fe9052f846c5a7d2123f
Qamar ali Khan <Qamarali.Khan@mrcooper.com>
2020-01-10 11:42:48 -0600

    first commit

---
+++ benchmarktest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Order;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace benchmarktest
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class Customers
+    {
+        private List<string> names = new List<string>() { "A","B","C"};
+        
+
+        [Benchmark(Baseline =true)]
+        public string ReturnbySingle()
+        {
+            return names.Single(x=>x=="C") ;
+        }
+
+        [Benchmark]
+        public string ReturnByforEachLoop()
+        {
+            foreach (var x in names)
+            {
+                if (x == "C")
+                {
+                    return x;
+                }
+
+            }
+            return "Not found";
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Customers>();
+        }
+    }
+
+
+}
